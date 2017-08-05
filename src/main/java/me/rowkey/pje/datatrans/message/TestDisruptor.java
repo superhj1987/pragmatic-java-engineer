271e51025755f16167b550e2c70edfe8047ca35c
superhj1987 <superhj1987@126.com>
2017-08-05 18:46:42 +0800

    add bigdata todo

---
+++ src/main/java/me/rowkey/pje/datatrans/message/TestDisruptor.java
@@ -0,0 +1,94 @@
+package me.rowkey.pje.datatrans.message;
+
+import com.lmax.disruptor.EventFactory;
+import com.lmax.disruptor.EventHandler;
+import com.lmax.disruptor.EventTranslatorOneArg;
+import com.lmax.disruptor.RingBuffer;
+import com.lmax.disruptor.dsl.Disruptor;
+
+import java.nio.ByteBuffer;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+
+public class TestDisruptor {
+    public void test() throws Exception {
+        TestEventFactory factory = new TestEventFactory();
+
+        ThreadFactory threadFactory = Executors.defaultThreadFactory();
+        Disruptor<TestEvent> disruptor = new Disruptor<>(factory, 1024, threadFactory);
+
+        disruptor.handleEventsWith(new TestEventHandler());
+
+        disruptor.start();
+
+        RingBuffer<TestEvent> ringBuffer = disruptor.getRingBuffer();
+
+        TestEventProducerWithTranslator producer = new TestEventProducerWithTranslator(ringBuffer);
+
+        ByteBuffer bb = ByteBuffer.allocate(8);
+        for (long l = 0; true; l++) {
+            bb.putLong(0, l);
+            producer.onData(bb);
+            Thread.sleep(1000);
+        }
+    }
+}
+
+class TestEventProducer {
+    private final RingBuffer<TestEvent> ringBuffer;
+
+    public TestEventProducer(RingBuffer<TestEvent> ringBuffer) {
+        this.ringBuffer = ringBuffer;
+    }
+
+    public void onData(ByteBuffer bb) {
+        long sequence = ringBuffer.next();  // Grab the next sequence
+        try {
+            TestEvent event = ringBuffer.get(sequence); // Get the entry in the Disruptor
+            // for the sequence
+            event.set(bb.getLong(0));  // Fill with data
+        } finally {
+            ringBuffer.publish(sequence);
+        }
+    }
+}
+
+
+class TestEventProducerWithTranslator {
+    private final RingBuffer<TestEvent> ringBuffer;
+
+    public TestEventProducerWithTranslator(RingBuffer<TestEvent> ringBuffer) {
+        this.ringBuffer = ringBuffer;
+    }
+
+    private static final EventTranslatorOneArg<TestEvent, ByteBuffer> TRANSLATOR =
+            new EventTranslatorOneArg<TestEvent, ByteBuffer>() {
+                public void translateTo(TestEvent event, long sequence, ByteBuffer bb) {
+                    event.set(bb.getLong(0));
+                }
+            };
+
+    public void onData(ByteBuffer bb) {
+        ringBuffer.publishEvent(TRANSLATOR, bb);
+    }
+}
+
+class TestEvent {
+    private long value;
+
+    public void set(long value) {
+        this.value = value;
+    }
+}
+
+class TestEventFactory implements EventFactory<TestEvent> {
+    public TestEvent newInstance() {
+        return new TestEvent();
+    }
+}
+
+class TestEventHandler implements EventHandler<TestEvent> {
+    public void onEvent(TestEvent event, long sequence, boolean endOfBatch) {
+        System.out.println("Event: " + event);
+    }
+}
---
+++ todo/bigdata/README.md
@@ -0,0 +1,5 @@
+# 大数据
+
+- Google paper:
+-
+
