702ce4b9c8b56745075293077afc673d0a5e9962
superhj1987 <superhj1987@126.com>
2017-08-21 10:46:02 +0800

    Enhancement : add rsa

--- source/src/main/java/me/rowkey/pje/security/JavaCiperExample.java
+++ source/src/main/java/me/rowkey/pje/security/JavaCiperExample.java
@@ -4,9 +4,13 @@ import javax.crypto.*;
 import javax.crypto.interfaces.DHPublicKey;
 import javax.crypto.spec.*;
 import java.security.*;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Bryant.Hang on 2017/8/9.
@@ -147,6 +151,39 @@ public class JavaCiperExample {
         cipher = Cipher.getInstance(secretKey.getAlgorithm());
         cipher.init(Cipher.DECRYPT_MODE, secretKey);
 
+        cipher.doFinal(encryData);
+    }
+
+    public static void testRSA() throws NoSuchAlgorithmException, InvalidKeySpecException, InvalidKeyException, NoSuchPaddingException, BadPaddingException, IllegalBlockSizeException {
+        KeyPairGenerator keyPairGen = KeyPairGenerator
+                .getInstance("RSA");
+        keyPairGen.initialize(1024);
+
+        KeyPair keyPair = keyPairGen.generateKeyPair();
+
+        // 公钥
+        byte[] publicKeyBytes = keyPair.getPublic().getEncoded();
+
+        // 私钥
+        byte[] privateKeyBytes = keyPair.getPrivate().getEncoded();
+
+        X509EncodedKeySpec x509KeySpec = new X509EncodedKeySpec(publicKeyBytes);
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        Key publicKey = keyFactory.generatePublic(x509KeySpec);
+
+        // 对数据加密
+        Cipher cipher = Cipher.getInstance(keyFactory.getAlgorithm());
+        cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+        String plainText = "plain text.";
+        byte[] encryData = cipher.doFinal(plainText.getBytes());
+
+        // 取得私钥
+        PKCS8EncodedKeySpec pkcs8KeySpec = new PKCS8EncodedKeySpec(privateKeyBytes);
+        Key privateKey = keyFactory.generatePrivate(pkcs8KeySpec);
+
+        // 对数据解密
+        cipher = Cipher.getInstance(keyFactory.getAlgorithm());
+        cipher.init(Cipher.DECRYPT_MODE, privateKey);
         cipher.doFinal(encryData);
     }
 }
