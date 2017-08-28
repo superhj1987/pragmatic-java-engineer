1eedf049bac18490789b47626ea1672847fc317b
superhj1987 <superhj1987@126.com>
2017-08-28 18:41:17 +0800

    Enhancement: add @ServletComponentScan to scan all servlet components

--- source/src/main/java/me/rowkey/pje/spring/boot/BootApplication.java
+++ source/src/main/java/me/rowkey/pje/spring/boot/BootApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  * Created by Bryant.Hang on 2017/8/28.
  */
+@ServletComponentScan
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class,
         MongoAutoConfiguration.class,
         MongoDataAutoConfiguration.class})
